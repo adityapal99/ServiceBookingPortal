258839eeddcc9adc2c72c07605f98a384912acb3
adityapal99 <adityapal2000.2013@gmail.com>
2022-07-14 22:07:59 +0530

    deploy repo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  ConnectedServices: {
+    Authorization: "https://authorizationmicroservice20220713175548.azurewebsites.net/",
+    Product: "https://productmicroservice20220713172150.azurewebsites.net/",
+    ServiceBooking: "https://servicebookingmicroservice20220713184004.azurewebsites.net/",
+    User: "https://usermicroservice20220713180703.azurewebsites.net/"
+  }
 };
